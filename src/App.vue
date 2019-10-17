4ea6a471cfe5c95ad91c2e42bb236976a74eb518
unknown <FrimpongAlbertAttakora>
2019-10-17 15:10:34 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <app-header></app-header>
+  <router-view></router-view>
+</div>
+</template>
+
+
+<script>
+import showBlog from './components/showBlog';
+import addBlog from './components/addBlog';
+import listBlog from './components/listBlog';
+import header from './components/header';
+
+
+export default {
+  components: {
+    'add-blog' : addBlog,
+    'show-blog' : showBlog,
+    'list-blog' : listBlog,
+    'app-header' : header,
+  },
+   data(){
+     return{
+
+     }
+   }
+}
+</script>
+
+
+<style>
+
+
+</style>
